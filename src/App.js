c7af83ae65dadcd7cff0d81a29922330e3f753c7
Lucy Zhao <lucyzhao.zlx@gmail.com>
2020-08-02 12:19:18 -0700

    Rearrange UI components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useReducer, useState, useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
-import { Button, CircularProgress, Grid, Card, CardContent, Box } from '@material-ui/core';
+import { Button, CircularProgress, Grid, Card, Box, Divider, Slide } from '@material-ui/core';
 import LinearProgressWithLabel from './progressBar';
 import './App.css';
 
@@ -10,8 +10,8 @@ const stateMachine = {
   initial: 'awaitingUpload',
   states: {
     awaitingUpload: { on: { startTransfer: 'transferring' } },
-    transferring: { on: { transferDone: 'complete' }, showLoadingIcon: true, showProgressBar: true },
-    complete: { on: { reset: 'awaitingUpload' }, showResult: true, showProgressBar: true },
+    transferring: { on: { transferDone: 'complete' }, showLoadingIcon: true, showResultSection: true },
+    complete: { on: { reset: 'awaitingUpload' }, showReset: true, showResultSection: true },
   }
 }
 
@@ -39,6 +39,8 @@ function App() {
       } else {
         console.err('invalid upload img type');
       }
+      // Reset file input so that onChange is always triggered
+      e.target.value = null;
     }
   }
 
@@ -119,131 +121,151 @@ function App() {
     setProgress(0);
   }
 
-  const { showLoadingIcon, showProgressBar, showResult } = stateMachine.states[state];
+  const { showLoadingIcon, showResultSection, showReset } = stateMachine.states[state];
 
 
   const cardStyleLeft = {
     marginTop: '5vw',
-    marginLeft: '10vw',
   }
   const cardStyleRight = {
     marginTop: '5vw',
-    marginRight: '10vw',
   }
-  const btnStyleLeft = {
-    marginLeft: '10vw',
-  }
-  const btnStyleRight = {
-    marginRight: '10vw',
+  const dividerStyle = {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: '15vw',
   }
 
   return (
     <div className="App">
-      {!showResult && <div>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Card style={cardStyleLeft}>
-              <Box
-                display="flex"
-                alignItems="center"
-                p={1}
-                m={1}
-                css={{ height: '30vw' }}>
-                <Grid item xs>
+      <Grid container spacing={7} direction="column" id="root-container">
+        {<Grid item id="section-upload">
+          <Grid container spacing={0} justify={'center'} id="section-upload-img">
+            <Grid item xs={5} id="section-upload-img-content">
+              <Card style={cardStyleLeft}>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p={1}
+                  css={{ height: '30vw' }}>
                   {<img alt="" src={contentImageUrl} ref={contentImageRef} />}
+                </Box>
+              </Card>
+            </Grid>
+            <Grid item xs={1} id="section-upload-btn-combine">
+              <Box marginTop={"5vw"}>
+                <Divider orientation="vertical" style={dividerStyle} />
+                <Button
+                  color="primary"
+                  aria-label="outlined primary"
+                  onClick={handleStyleTransfer}
+                  disabled={!(contentImageUrl && styleImageUrl)}
+                >
+                  Combine
+                </Button>
+                <Divider orientation="vertical" style={dividerStyle} />
+              </Box>
+            </Grid>
+            <Grid item xs={5} id="section-upload-img-style">
+              <Card style={cardStyleRight}>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p={1}
+                  css={{ height: '30vw' }}>
+                  {<img alt="" src={styleImageUrl} ref={styleImageRef} />}
+                </Box>
+              </Card>
+            </Grid>
+          </Grid>
+          <Grid container spacing={3} id="section-upload-btns">
+            <input
+              id="content-file-input"
+              className="file-input"
+              type="file"
+              accept="image/*"
+              ref={contentInputRef}
+              onChange={e => handleUpload(e, 'content')}
+            />
+            <input
+              id="style-file-input"
+              className="file-input"
+              type="file"
+              accept="image/*"
+              ref={styleInputRef}
+              onChange={e => handleUpload(e, 'style')}
+            />
+            <Grid item xs id="section-upload-btn-content">
+              <label htmlFor="content-file-input">
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  aria-label="outlined primary"
+                  onClick={() => contentInputRef.current.click()}
+                >
+                  Upload Content Image
+              </Button>
+              </label>
+            </Grid>
+            <Grid item xs id="section-upload-btn-style">
+              <label htmlFor="style-file-input">
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  aria-label="outlined primary"
+                  onClick={() => styleInputRef.current.click()}
+                >
+                  Upload Style Image
+              </Button>
+              </label>
+            </Grid>
+          </Grid>
+        </Grid>}
+        <Grid item id="section-result">
+          <Slide direction="up" in={showResultSection} mountOnEnter unmountOnExit>
+            <Box boxShadow={4} p={"5vw"}>
+              <Grid
+                container
+                direction="column"
+                spacing={5}
+                justify="center"
+                alignItems="center">
+                <Grid item>
+                  <Card>
+                    <canvas
+                      id="canvas"
+                      ref={canvasRef}
+                      width={112}
+                      height={112}
+                    />
+                  </Card>
                 </Grid>
-              </Box>
-            </Card>
-          </Grid>
-          <Grid item xs={6}>
-            <Card style={cardStyleRight}>
-              <Box
-                display="flex"
-                alignItems="center"
-                p={1}
-                m={1}
-                css={{ height: '30vw' }}>
-                {<img alt="" src={styleImageUrl} ref={styleImageRef} />}
-              </Box>
-            </Card>
-          </Grid>
+                <Grid item>
+                  {showLoadingIcon &&
+                    <CircularProgress />
+                  }
+                </Grid>
+                <Grid item>
+                  {<Box width="30vw">
+                    <LinearProgressWithLabel value={progress} />
+                  </Box>}
+                </Grid>
+                <Grid item>
+                  {showReset &&
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      aria-label="outlined primary"
+                      onClick={handleReset}
+                    >
+                      Reset
+                  </Button>}
+                </Grid>
+              </Grid>
+            </Box>
+          </Slide>
         </Grid>
-        <Grid container spacing={3}>
-          <input
-            id="content-file-input"
-            className="file-input"
-            type="file"
-            accept="image/*"
-            ref={contentInputRef}
-            onChange={e => handleUpload(e, 'content')}
-          />
-          <input
-            id="style-file-input"
-            className="file-input"
-            type="file"
-            accept="image/*"
-            ref={styleInputRef}
-            onChange={e => handleUpload(e, 'style')}
-          />
-          <Grid item xs>
-            <label htmlFor="content-file-input">
-              <Button
-                style={btnStyleLeft}
-                color="primary"
-                aria-label="outlined primary"
-                onClick={() => contentInputRef.current.click()}
-              >
-                Upload Content Image
-              </Button>
-            </label>
-          </Grid>
-          <Grid item xs>
-            <label htmlFor="style-file-input">
-              <Button
-                style={btnStyleRight}
-                color="primary"
-                aria-label="outlined primary"
-                onClick={() => styleInputRef.current.click()}
-              >
-                Upload Style Image
-              </Button>
-            </label>
-          </Grid>
-        </Grid>
-        {contentImageUrl && styleImageUrl &&
-          <Button
-            color="primary"
-            aria-label="outlined primary"
-            onClick={handleStyleTransfer}
-          >
-            Combine
-        </Button>}
-      </div>}
-      {<canvas
-        id="canvas"
-        ref={canvasRef}
-        width={112}
-        height={112}
-        style={{
-          border: '2px solid #000',
-          marginTop: 10,
-        }}
-      />}
-      {showResult && <div>
-        <Button
-          color="primary"
-          aria-label="outlined primary"
-          onClick={handleReset}
-        >
-          Reset
-        </Button>
-      </div>}
-      {showLoadingIcon && <div>
-        <CircularProgress />
-      </div>}
-      {showProgressBar && <div>
-        <LinearProgressWithLabel value={progress} />
-      </div>}
+      </Grid>
     </div>
   );
 }
